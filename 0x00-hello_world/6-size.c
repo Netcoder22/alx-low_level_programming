ca4b43063c9d35136f383ae879df9ee25335e1f4
Kwaku <kwaku.asare09@gmail.com>
2023-02-21 12:18:07 -0800

    6 size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,7 @@
  *
  * Return: Always 0.
  */
-int main(void)
+int main()
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
@@ -15,3 +15,4 @@ int main(void)
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
 	return (0);
+}
