f84564955a19625cb44931fbfd2e5683a20cc671
Kwaku <kwaku.asare09@gmail.com>
2023-03-02 19:33:17 -0800

    0-putchar correction

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,24 +1,10 @@
 #include "main.h"
-#include <unistd.h>
 
 /**
- * _putchar - Writes a character to the standard output (stdout)
- *
- * @c: The character to write
- *
- * Return: On success, the number of characters written is returned.
- *         On error, -1 is returned.
+ * print_alphabet - Entry point
+ * Return:0
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 
-/**
- * print_alphabet - Prints the alphabet in lowercase followed by a newline
- *
- * Return: void
- */
 void print_alphabet(void)
 {
 	char c = 'a';
@@ -27,19 +13,8 @@ void print_alphabet(void)
 	{
 		_putchar(c);
 		c++;
+
 	}
-
 	_putchar('\n');
-}
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	print_alphabet();
-
-	return (0);
 }
