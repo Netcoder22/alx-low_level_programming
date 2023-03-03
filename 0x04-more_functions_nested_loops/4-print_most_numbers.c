2eac1a1adc9ff09cba293b7afadf3ce80ee70ea7
Kwaku <kwaku.asare09@gmail.com>
2023-03-03 13:30:49 -0800

    4-print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,23 +1,17 @@
-#include "main.h"
+#include "holberton.h"
 /**
- * print_most_numbers - prints all numbers from 0 to 9 except 2 and 4.
+ * print_most_numbers - print all number from 0 to 9 expext 2 and 4.
  *
  * Return:void
  */
 void print_most_numbers(void)
 {
-	int i;
+int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 48; i <= 57; i++)
 	{
-		if (i == 2 || i == 4)
-			{
-				continue;
-			}
-			else
-			{
-				_putchar(i + '0');
-			}
+		if ((i != 50) && (i != 52))
+			_putchar(i);
 	}
-	_putchar('\n');	
+_putchar('\n');
 }
