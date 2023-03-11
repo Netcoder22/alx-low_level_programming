e6cfbbe00d9973c5bf8a12bafcb95a63493971ac
Kwaku <kwaku.asare09@gmail.com>
2023-03-10 19:39:32 -0800

    0-reset-to-98

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * _putchar - Writes a character to the standard output (stdout)
+ *
+ * @c: The character to write
+ *
+ * Return: On success, the number of characters written is returned.
+ *         On error, -1 is returned.
+ */
+
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}
